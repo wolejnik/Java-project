4fe7b26388bc34de4ba2c4b3553601d76c683533
Wojtek <olejnik.wojciech1@gmail.com>
2018-08-14 22:55:53 +0200

    zmiana BankAccountTest

--- ProjectBankAccount/src/bankAccount/BankAccountTest.java
+++ ProjectBankAccount/src/bankAccount/BankAccountTest.java
@@ -12,6 +12,7 @@ public class BankAccountTest {
 	
 	public static void main(String[] args)
 	{
+		
 		Account k = new Account("Wojtek", 1234, 1000);
 		k.show();
 		k.setBalance(500);
@@ -24,6 +25,20 @@ public class BankAccountTest {
 		k2.show();
 		k2.getMoney(3000);
 		k2.show();
+		
+		/*
+		 * salary :)
+		 */
+		
+		Account[] bank = new Account[3];
+		bank[0] = new Account("Jan", 1010, 800);
+		bank[1] = new Account("Tomek", 1111, 900);
+		bank[2] = new Account("Ola", 2222, 700);
+		
+		for(Account a : bank)
+		{
+			a.show();
+		}
 	}
 
 }
